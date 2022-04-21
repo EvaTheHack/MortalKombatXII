f0c4727955b6e9f0d758cd430aa63c53fedaf75a
DESKTOP-BEVMH6N\Nachtigal <evathehack@gmail.com>
2022-04-21 23:55:44 +0300

    encapsulate http requests and parsing to util

---
+++ MortalKombatXII.ClientConsole/Services/HttpUtil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+
+namespace MortalKombatXII.ClientConsole.Services
+{
+    public class HttpUtil
+    {
+        private static readonly HttpClient _client = new HttpClient();
+
+        public static string Get(string url)
+        {
+            var response = _client.GetAsync(url).Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Server is unreachable");
+            }
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
+        public static string Post(string url)
+        {
+            var response = _client.PostAsync(url, null).Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Server is unreachable");
+            }
+            return response.Content.ReadAsStringAsync().Result;
+        }
+    }
+}
